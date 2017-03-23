3ee3eebf63793c5005b33d2e72ac34413bb269fd
wener <wenermail@gmail.com>
2017-03-23 19:37:19 +0800

    为 php 5,7 添加 WWW_ROOT 配置项, 添加 piwik 暂时不自动 build zentao , 因为下载的东西太大了

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 { [ -f ../.build/init.sh ] && . ../.build/init.sh; } || true;
 buildreport || exit
 
-names=${1:-"base edge ubuntu nginx java node builder autossh zentao caddy pdns nmap"}
+names=${1:-"base edge ubuntu nginx java node builder autossh piwik caddy pdns nmap"}
 builddocker_dirs $names
 
--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -e
 set -o xtrace
-chown -R nobody:nobody /var/www/html
+chown -R nobody:nobody ${WWW_ROOT:-/var/www/html}
+[ -z "$WWW_ROOT" ] || sed -i -e "s#/var/www/html#$WWW_ROOT#g" /etc/nginx/nginx.conf
+
 php-fpm -t && nginx -t
 
 php-fpm
