d0ebb0a90ffe6ac91e0ff7b45a990d400f70bfe9
wener <wenermail@gmail.com>
2017-08-30 22:16:57 +0800

    Edge add bash, add asterisk

---
+++ asterisk/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+{ [ -f .build/init.sh ] && . .build/init.sh; } || true;
+{ [ -f ../.build/init.sh ] && . ../.build/init.sh; } || true;
+buildreport || exit
+
+builddocker_init_ver asterisk
+vers=${1:-"asterisk edge"}
+builddocker_vers $vers
