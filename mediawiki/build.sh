8ed6dbfd1c2b6bd75597ea9f744faca20bbdc07f
wener <wenermail@gmail.com>
2017-12-07 02:18:47 +0800

    1. update to 3.7 2. add mediawiki:parsoid 3. node add pax fix 4. php fix iconv

---
+++ mediawiki/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+{ [ -f .build/init.sh ] && . .build/init.sh; } || true;
+{ [ -f ../.build/init.sh ] && . ../.build/init.sh; } || true;
+buildreport || exit
+
+builddocker_init_ver mediawiki
+vers=${1:-"parsoid"}
+builddocker_vers $vers
