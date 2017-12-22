a6698e5e296bb4946e6656b47926e17b26d8f8a2
wener <wenermail@gmail.com>
2017-12-22 18:20:24 +0800

    add java:stack

---
+++ java/stack/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+# setup ssh
+[ -f /etc/ssh/ssh_host_rsa_key ] || {
+    ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N '' > /dev/null
+    PASSWORD=${PASSWORD:-$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | head -c 6)}
+    USERNAME=root
+    echo "$USERNAME:$PASSWORD" | chpasswd
+    echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+    echo "OpenSSH in Docker
+User: $USERNAME
+Pass: $PASSWORD
+"
+}
+
+/usr/sbin/sshd -D -h /etc/ssh/ssh_host_rsa_key $*
