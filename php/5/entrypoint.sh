8869312fb63b8da68892a709d0de6592b1a1f3a7
wener <wenermail@gmail.com>
2018-05-08 10:34:17 +0800

    1. fix php:5 entrypoint 2. add 5-app

--- php/5/entrypoint.sh
+++ php/5/entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 set -e
-set -o xtrace
-chown -R nobody:nobody ${WWW_ROOT:-/var/www/html}
-[ -z "$WWW_ROOT" ] || sed -i -e "s#/var/www/html#$WWW_ROOT#g" /etc/nginx/nginx.conf
 
-php-fpm -t && nginx -t
+if [ $# = 0 ] ; then
+    set -o xtrace
+    chown -R nobody:nobody ${WWW_ROOT:-/var/www/html}
+    [ -z "$WWW_ROOT" ] || sed -i -e "s#/var/www/html#$WWW_ROOT#g" /etc/nginx/nginx.conf
 
-php-fpm
-nginx -g 'daemon off;'
+    php5-fpm -t && nginx -t
+
+    php5-fpm
+    nginx -g 'daemon off;'
+else
+  "$@"
+fi
