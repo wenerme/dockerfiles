6d63a32c8f9cbee71d9c7adb67332f7b437c08db
wener <wenermail@gmail.com>
2018-07-05 11:51:35 +0800

    add python and alembic

---
+++ python/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+{ [ -f .build/init.sh ] && . .build/init.sh; } || true;
+{ [ -f ../.build/init.sh ] && . ../.build/init.sh; } || true;
+buildreport || exit
+
+builddocker_init_ver python
+vers=${1:-"python 2 alembic"}
+builddocker_vers $vers
