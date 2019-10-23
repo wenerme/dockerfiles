96cb422ebb852c556b3076d4498288076797377f
wener <wenermail@gmail.com>
2019-10-23 09:14:40 +0800

    add base:openrc; add app

---
+++ app/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+{ [ -f .build/init.sh ] && . .build/init.sh; } || true;
+{ [ -f ../.build/init.sh ] && . ../.build/init.sh; } || true;
+buildreport || exit
+
+builddocker_init_ver app
+# By default not build base, because base will always build no matter it changed or not
+vers=${@:-"app java8"}
+[ "$BUILD_ALL" != "" ] && vers="$vers_all"
+
+builddocker_vers $vers
