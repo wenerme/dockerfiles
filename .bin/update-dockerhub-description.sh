77454bab9c70e76a6057258c386f87b98408cb66
wener <wenermail@gmail.com>
2020-03-31 03:56:39 +0800

    gitlab ci

---
+++ .bin/update-dockerhub-description.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# Based on https://github.com/peter-evans/dockerhub-description
+# README_FILEPATH
+# DOCKERHUB_USERNAME
+# DOCKERHUB_PASSWORD
+# DOCKERHUB_REPOSITORY
+
+# Set the default path to README.md
+README_FILEPATH=${README_FILEPATH:="./README.md"}
+
+[ -d "$README_FILEPATH" ]
+
+# Check the file size
+if [ $(wc -c <${README_FILEPATH}) -gt 25000 ]; then
+  echo "File size exceeds the maximum allowed 25000 bytes"
+  exit 1
+fi
+
+# Acquire a token for the Docker Hub API
+echo "Acquiring token"
+LOGIN_PAYLOAD="{\"username\": \"${DOCKERHUB_USERNAME}\", \"password\": \"${DOCKERHUB_PASSWORD}\"}"
+TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d ${LOGIN_PAYLOAD} https://hub.docker.com/v2/users/login/ | jq -r .token)
+
+# Send a PATCH request to update the description of the repository
+echo "Sending PATCH request"
+REPO_URL="https://hub.docker.com/v2/repositories/${DOCKERHUB_REPOSITORY}/"
+RESPONSE_CODE=$(curl -s --write-out %{response_code} --output /dev/null -H "Authorization: JWT ${TOKEN}" -X PATCH --data-urlencode full_description@${README_FILEPATH} ${REPO_URL})
+echo "Received response code: $RESPONSE_CODE"
+
+if [ $RESPONSE_CODE -eq 200 ]; then
+  exit 0
+else
+  exit 1
+fi
