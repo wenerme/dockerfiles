8dcc0739c3065d92e74506fb36a6b336f93aaabc
wener <wenermail@gmail.com>
2020-06-07 02:32:59 +0800

    add alpine-image-builder

---
+++ alpine-image-builder/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+{ [ -f .build/init.sh ] && . .build/init.sh; } || true;
+{ [ -f ../.build/init.sh ] && . ../.build/init.sh; } || true;
+buildreport || exit
+
+builddocker_init_ver alpine-image-builder
+# By default not build base, because base will always build no matter it changed or not
+vers=${@:-"aarch64"}
+# vers_all="bash util builder svc sys man armhf aarch64 s390x ppc64le x86"
+vers_all="bash openrc util builder svc sys man armhf aarch64 s390x x86"
+[ "$BUILD_ALL" != "" ] && vers="$vers_all"
+
+builddocker_vers $vers
