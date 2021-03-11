3e70c759e8d7c0345cd23f001939f60dd1453427
wener <wenermail@gmail.com>
2021-03-12 01:34:28 +0800

    add more gitlab-runner; add verdaccio

---
+++ gitlab-runner/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+{ [ -f .build/init.sh ] && . .build/init.sh; } || true;
+{ [ -f ../.build/init.sh ] && . ../.build/init.sh; } || true;
+buildreport || exit
+
+builddocker_init_ver gitlab-runner
+vers=${1:-gitlab-runner}
+builddocker_vers $vers
