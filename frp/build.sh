b2d91db5669ff98135cbf75e336a0d4c66c99cfc
wener <wenermail@gmail.com>
2021-11-20 21:11:46 +0800

    frp: tag frps, frpc, version

--- frp/build.sh
+++ frp/build.sh
@@ -6,3 +6,11 @@ buildreport || exit
 builddocker_init_ver frp
 vers=${1:-frp frps frpc}
 builddocker_vers $vers
+
+ver=v$(docker run --rm -it wener/frp frps -v | tr -d '[:space:]')
+docker tag wener/frp:frps wener/frp:$ver
+docker tag wener/frp:frps wener/frps:$ver
+docker tag wener/frp:frpc wener/frpc:$ver
+docker push wener/frp:$ver
+docker push wener/frps:$ver
+docker push wener/frpc:$ver
