5c740a7704b646a892faff15ebd8ab230b853e54
wener <wenermail@gmail.com>
2022-04-02 07:22:28 +0800

    node tag version

--- node/build.sh
+++ node/build.sh
@@ -7,3 +7,14 @@ builddocker_init_ver node
 vers=${1:-"node"}
 
 builddocker_vers $vers
+
+ver=$(docker run --rm -it wener/node node -v | tr -d '\r\n')
+ver_short=${ver%.*}
+ver_major=${ver_short%.*}
+
+[[ $vers =~ "node" ]] && {
+  for v in $ver $ver_short $ver_major; do
+    docker tag wener/node $DOCKER_REGISTRY/$GROUP/node:"$v"
+    docker push $DOCKER_REGISTRY/$GROUP/node:"$v"
+  done
+}
